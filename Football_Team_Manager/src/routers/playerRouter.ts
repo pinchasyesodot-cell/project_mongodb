6ace8e3433820cba8850c0dc2c5c24af2141ab0b
Pinchas Lifshitz <pinchas.yesodot@gmail.com>
2026-07-14 14:58:53 +0300

    feat: add player router implementation

---
+++ Football_Team_Manager/src/routers/playerRouter.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { PlayerController } from "../controller/player.controller.js";
+
+class PlayerRouter {
+    public router: Router;
+    constructor() {
+        this.router = Router();
+        this.initRoutes();
+    }
+    private initRoutes = (): void => {
+        this.router.post("/", PlayerController.createPlayer);
+        this.router.get("/team/:teamId", PlayerController.getPlayerByTeam);
+        this.router.get("/search", PlayerController.getPlayersByName);
+        this.router.get("/number/:teamId/", PlayerController.getPlayerByNumber);
+        this.router.put("/:playerId/transfer", PlayerController.transferPlayer);
+        this.router.delete("/:playerId", PlayerController.deletePlayer);
+    };
+}
+
+export default new PlayerRouter().router;
