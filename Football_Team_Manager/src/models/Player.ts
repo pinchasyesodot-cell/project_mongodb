3d603cada5063a77615d03c898268fff5c9822c4
Pinchas Lifshitz <pinchas.yesodot@gmail.com>
2026-07-14 14:15:52 +0300

    refactor: change variables to lowercase, add timestamps and transform

--- Football_Team_Manager/src/models/Player.ts
+++ Football_Team_Manager/src/models/Player.ts
@@ -1,14 +1,28 @@
 import { model, Schema } from "mongoose";
 import type { Player } from "../interfaces/Player.js";
 
-const Player = new Schema<Player>({
-    id: { type: String, required: true, unique: true, minlength: 9, maxlength: 9 },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    Nationality: { type: String, required: true },
-    Number: { type: Number, required: true },
-    Cost: { type: Number, required: true },
-    Team: { type: String, required: true },
-});
-
+const Player = new Schema<Player>(
+    {
+        playerId: { type: String, required: true, unique: true, minlength: 9, maxlength: 9 },
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
+        nationality: { type: String, required: true },
+        number: { type: Number, required: true },
+        cost: { type: Number, required: true },
+        teamId: { type: String },
+    },
+    {
+        timestamps: true,
+        toJSON: {
+            transform: (_doc, ret: Record<string, any>) => {
+                delete ret.__v;
+                delete ret._id;
+                delete ret.createdAt;
+                delete ret.updatedAt;
+                return ret;
+            },
+        },
+    }
+);
+Player.index({ nationality: 1, teamId: 1 });
 export const PlayerModel = model<Player>("Player", Player);
