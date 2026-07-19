a244959e99a4a93bea730063e64236dc9668aa95
Pinchas Lifshitz <pinchas.yesodot@gmail.com>
2026-07-19 13:20:58 +0300

    feat(logger): initialize winston logger with json and file transports

--- Football_Team_Manager/src/config/env.ts
+++ Football_Team_Manager/src/config/env.ts
@@ -3,3 +3,4 @@ import env from "env-var";
 
 export const PORT = env.get("PORT").required().default("3000").asPortNumber();
 export const MONGO_URI = env.get("MONGO_URI").required().asString();
+export const NODE_ENV = env.get("NODE_ENV").asString()
